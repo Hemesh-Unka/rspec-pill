11827b87d2c57a929db6618512a8722a9ba6e7dc
Hemesh-Unka <hemeshunka@gmail.com>
2018-02-14 09:16:24 +0000

    Added greeter and greeter_spec files - both tests pass

---
+++ spec/greeter_spec.rb
@@ -0,0 +1,15 @@
+require 'greeter'
+
+describe 'Greeter' do
+  it 'greets Hemesh' do
+    expect(greet('Hemesh')).to eq 'Hello, Hemesh, how are you today?'
+  end
+
+  it 'greets Hansa' do
+    expect(greet('Hansa')).to eq 'Hello, Hansa, how are you today?'
+  end
+
+  # name = 'Hemesh' # Given
+  # result = greet(name) # When
+  # expect(result).to eq 'Hello, Rico, how are you doing today' # Then
+end
